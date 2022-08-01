8a8a24781d7207687d6b27223c819726c97dbb6c
A Fang <hunglexuan92@live.com>
2022-08-01 22:06:48 +0700

    my

--- models/User.js
+++ models/User.js
@@ -7,4 +7,5 @@ const UserSchema = mongoose.Schema({
     avatar: {type: String, default: 'publics/statics/default-avatar-profile-image-vector-social-media-user-icon-potrait-182347582.jpg'}
 }, {collection: 'users', timestamps: true});
 
+
 module.exports.User = mongoose.model('users', UserSchema);
